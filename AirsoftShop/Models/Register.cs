63d5974e3bf61b33f1fdfeb6bd68d03737e5de0e
P1stak <shik213mainl.ru>
2025-07-05 17:09:58 +0700

    доработка валидации логина и регистрации на стороне сервера

--- AirsoftShop/Models/Register.cs
+++ AirsoftShop/Models/Register.cs
@@ -4,20 +4,18 @@ namespace AirsoftShop.Models
 {
     public class Register
     {
-        [Required(ErrorMessage = "Введите Email")]
-        [EmailAddress(ErrorMessage = "Некорректный email")]
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Введите пароль")]
-        [DataType(DataType.Password)]
-        [MinLength(6, ErrorMessage = "Минимум 6 символов")]
-        [MaxLength(20, ErrorMessage = "Максимум 20 символов")]
+        [Required]
+        [StringLength(25, MinimumLength = 6)]
         [RegularExpression(@"^(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).{6,}$",
             ErrorMessage = "Пароль должен содержать 1 заглавную букву, 1 цифру и 1 спецсимвол")]
         public string Password { get; set; }
 
-        [Required(ErrorMessage = "Подтвердите пароль")]
-        [DataType(DataType.Password)]
+        [Required]
+        [StringLength(25, MinimumLength = 6)]
         [Compare("Password", ErrorMessage = "Пароли не совпадают")]
         public string ConfirmPassword { get; set; }
     }
