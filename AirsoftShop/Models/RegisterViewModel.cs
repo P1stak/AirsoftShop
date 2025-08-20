47bdaa3704587c49ee7074f95532a56250f3f0a7
P1stak <shik213mainl.ru>
2025-08-20 11:06:22 +0700

    исправлена проблема с регистрацией из-за кириллицы

--- AirsoftShop/Models/RegisterViewModel.cs
+++ AirsoftShop/Models/RegisterViewModel.cs
@@ -8,26 +8,32 @@ namespace AirsoftShop.Models
         [EmailAddress(ErrorMessage = "Некорректный формат Email")]
         public string Email { get; set; }
 
+
         [Required]
         [StringLength(25, MinimumLength = 6)]
         [RegularExpression(@"^(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).{6,}$",
-        ErrorMessage = "Пароль должен содержать 1 заглавную букву, 1 цифру и 1 спецсимвол")]
+            ErrorMessage = "Пароль должен содержать 1 заглавную букву, 1 цифру и 1 спецсимвол")]
         public string Password { get; set; }
+
 
         [Required]
         [StringLength(25, MinimumLength = 6)]
         [Compare("Password", ErrorMessage = "Пароли не совпадают")]
         public string ConfirmPassword { get; set; }
 
+
         [Required]
         [Phone(ErrorMessage = "Некорректный формат телефона")]
         [RegularExpression(
-        @"^(\+7|7|8)?[\s\-]?\(?[0-9]{3}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$",
-        ErrorMessage = "Некорректный формат телефона. Пример: +7(XXX)XXX-XX-XX или 8XXXXXXXXXX")]
+            @"^(\+7|7|8)?[\s\-]?\(?[0-9]{3}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$",
+            ErrorMessage = "Некорректный формат телефона. Пример: +7(XXX)XXX-XX-XX или 8XXXXXXXXXX")]
         public string Phone { get; set; }
 
+
         [Required(ErrorMessage = "Поле обязательно")]
-        [RegularExpression(@"^[А-ЯЁа-яё]+$", ErrorMessage = "Имя пользователя должно содержать только кириллические символы")]
+        [StringLength(50, MinimumLength = 2)]
+        [RegularExpression(@"^[a-zA-Zа-яА-ЯёЁ\s]+$",
+            ErrorMessage = "Можно использовать только русские/английские буквы и пробелы")]
         public string UserName { get; set; }
 
         public string? Address { get; set; }
